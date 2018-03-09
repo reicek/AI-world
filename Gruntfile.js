adaf447a9be150ce6209c85f2e96f32241be873f
reicek <reicek@gmail.com>
2018-03-09 11:56:58 -0500

    v1.2.8 - code climate - grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,103 +1,105 @@
 'use strict';
 
+const gruntConfig = {
+    express: {
+        options: {
+            debug: true,
+            port: '8080'
+        },
+        app: {
+            options: {
+                script: 'server.js',
+                node_env: 'development'
+            }
+        },
+    },
+
+    jshint: {
+        options: {
+            jshintrc: '.jshintrc',
+            reporterOutput: '',
+            reporter: require('jshint-stylish')
+        },
+        all: [
+            'src/scripts/**/*.js',
+        ],
+    },
+
+    vulcanize: {
+        default: {
+            options: {
+                inlineScripts: true,
+                inlineCss: true,
+                stripComments: true,
+                stripExcludes: true,
+            },
+            files: {
+                'docs/index.html': 'src/index.html'
+            },
+        },
+    },
+
+    watch: {
+        backend: {
+            files: [
+                'server.js',
+            ],
+            tasks: [
+                'jshint',
+            ],
+        },
+
+        modules: {
+            files: [
+                'node_modules'
+            ],
+            tasks: [
+                'vulcanize',
+            ],
+        },
+
+        scripts: {
+            files: [
+                'src/**/*.js',
+                'specs/**/*.js',
+            ],
+            tasks: [
+                'jshint',
+                'vulcanize',
+            ]
+        },
+
+        css: {
+            files: [
+                'src/**/*.css',
+            ],
+            tasks: [
+                'vulcanize',
+            ]
+        },
+
+        html: {
+            files: [
+                'src/**/*.html',
+            ],
+            tasks: [
+                'vulcanize',
+            ]
+        },
+
+        options: {
+            livereload: true
+        }
+    },
+};
+
 const tasks = grunt => {
     grunt.loadNpmTasks('grunt-express-server');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-vulcanize');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.initConfig({
-        express: {
-            options: {
-                debug: true,
-                port: '8080'
-            },
-            app: {
-                options: {
-                    script: 'server.js',
-                    node_env: 'development'
-                }
-            },
-        },
-
-        jshint: {
-            options: {
-                jshintrc: '.jshintrc',
-                reporterOutput: '',
-                reporter: require('jshint-stylish')
-            },
-            all: [
-                'src/scripts/**/*.js',
-            ],
-        },
-
-        vulcanize: {
-            default: {
-                options: {
-                    inlineScripts: true,
-                    inlineCss: true,
-                    stripComments: true,
-                    stripExcludes: true,
-                },
-                files: {
-                    'docs/index.html': 'src/index.html'
-                },
-            },
-        },
-
-        watch: {
-            backend: {
-                files: [
-                    'server.js',
-                ],
-                tasks: [
-                    'jshint',
-                ],
-            },
-
-            modules: {
-                files: [
-                    'node_modules'
-                ],
-                tasks: [
-                    'vulcanize',
-                ],
-            },
-
-            scripts: {
-                files: [
-                    'src/**/*.js',
-                    'specs/**/*.js',
-                ],
-                tasks: [
-                    'jshint',
-                    'vulcanize',
-                ]
-            },
-
-            css: {
-                files: [
-                    'src/**/*.css',
-                ],
-                tasks: [
-                    'vulcanize',
-                ]
-            },
-
-            html: {
-                files: [
-                    'src/**/*.html',
-                ],
-                tasks: [
-                    'vulcanize',
-                ]
-            },
-
-            options: {
-                livereload: true
-            }
-        },
-    });
+    grunt.initConfig(gruntConfig);
 
     grunt.registerTask('default', [
         'express',
